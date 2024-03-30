7eebdd651a445f0b09e9fde0be9265390f30adc3
Ken Luzuriaga <kenluzuriaga0@hotmail.com>
2024-03-29 22:39:34 -0500

    add: create project

---
+++ ticket-power-back/src/main/java/nek/ticketpower/TicketPowerApplication.java
@@ -0,0 +1,13 @@
+package nek.ticketpower;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketPowerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketPowerApplication.class, args);
+	}
+
+}
---
+++ ticket-power-back/src/test/java/nek/ticketpower/TicketPowerApplicationTests.java
@@ -0,0 +1,13 @@
+package nek.ticketpower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicketPowerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
