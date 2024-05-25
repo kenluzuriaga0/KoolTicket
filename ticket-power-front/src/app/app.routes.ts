2637f15e031bd3e3cee793141bbbc9cf5e0ca410
Ken luzuriaga <kenluzuriaga0@hotmail.com>
2024-05-25 18:23:23 -0500

    add: Not found page

--- ticket-power-front/src/app/app.routes.ts
+++ ticket-power-front/src/app/app.routes.ts
@@ -2,10 +2,12 @@ import { Routes } from '@angular/router';
 import { LoginComponent } from './pages/login/login.component';
 import { SelectBranchComponent } from './pages/select-branch/select-branch.component';
 import { TicketCallComponent } from './pages/ticket-call/ticket-call.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
 
 export const routes: Routes = [
     { path: '', component: LoginComponent },
     { path: 'login', component: LoginComponent },
     { path: 'branch', component: SelectBranchComponent },
     { path: 'call', component: TicketCallComponent }, // TODO: Colocar un child ":branchId"
+    { path: '**', component: NotFoundComponent },
 ];
