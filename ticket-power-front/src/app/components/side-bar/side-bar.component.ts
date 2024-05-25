d3ab26badddcbf8585691ce60f29193e1f82bfcf
Ken luzuriaga <kenluzuriaga0@hotmail.com>
2024-05-25 18:23:09 -0500

    imp: Animation in side-bar through burger button

--- ticket-power-front/src/app/components/side-bar/side-bar.component.ts
+++ ticket-power-front/src/app/components/side-bar/side-bar.component.ts
@@ -1,13 +1,31 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { animate, state, style, transition, trigger } from '@angular/animations';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule,],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.css'
+  styleUrl: './side-bar.component.css',
+  animations: [
+    trigger('sidebarAnimation', [
+      state('open', style({
+        transform: 'translateX(0)'
+      })),
+      state('closed', style({
+        transform: 'translateX(-100%)'
+      })),
+      transition('open <=> closed', [
+        animate('0.3s')
+      ])
+    ])
+  ]
 })
 export class SideBarComponent {
+  sidebarState: 'open' | 'closed' = 'open';
 
+  toggleSidebar() {
+    this.sidebarState = this.sidebarState === 'open' ? 'closed' : 'open';
+  }
 }
