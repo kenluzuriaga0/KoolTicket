9b9c545d0f4548a46eb39fc6a82befad51a89aac
Ken luzuriaga <kenluzuriaga0@hotmail.com>
2024-05-26 14:20:08 -0500

    imp: Wait list Page

--- ticket-power-front/src/app/pages/ticket-wait-list/ticket-wait-list.component.ts
+++ ticket-power-front/src/app/pages/ticket-wait-list/ticket-wait-list.component.ts
@@ -9,5 +9,18 @@ import { CommonModule } from '@angular/common';
   styleUrl: './ticket-wait-list.component.css'
 })
 export class TicketWaitListComponent {
+  lista = [
+    {name: 'Turno L1', caja: 'Caja 2'},
+    {name: 'Turno C2', caja: 'Caja 1'},
+    {name: 'Turno C3', caja: 'Caja 4'},
+    {name: 'Turno C4', caja: 'Caja 3'},
+    {name: 'Turno C5', caja: 'Caja 2'},
+    {name: 'Turno C6', caja: 'Caja 1'},
+    {name: 'Turno C7', caja: 'Caja 1'},
+    {name: 'Turno C8', caja: 'Caja 1'},
+    {name: 'Turno C9', caja: 'Caja 4'},
+    {name: 'Turno C7', caja: 'Caja 1'},
+    {name: 'Turno C7', caja: 'Caja 1'},
 
+  ]
 }
